c2e43a937c5c34ddd161364d970e384b9774afed
JavierJCC <javisever2@gmail.com>
2017-09-07 23:46:26 -0500

    Finalizada
    Interrupciones que finaliza en 20 segundo

--- practicaPTR1.c
+++ practicaPTR1.c
@@ -4,25 +4,38 @@
 #include <dos.h>
 #include <math.h>
 
-unsigned frecuencia=65563;
+void confi(void);
+void restablecervalores(void);
 
-void interrupt (*viejo)();
+unsigned frecuencia=65000;
+unsigned int i=0;
+unsigned int segundos=0;
+unsigned int limite=0;
+unsigned int limitef=0;
+unsigned int ciclos=0;
+unsigned int anterior=0;
+unsigned int Port;
+static int k=1;
+
+void interrupt far (*viejo)();
+void interrupt funcionInterrunpe();
 
 /*Configuramos el vector y realizamos el divisor de frecuencia*/
-void configuracion(){
-	int contador;
+void confi(){
 	int contador_lo;
 	int contador_hi;
-
+	int contador;
+		printf("configuramos");
 		/*Guardamos el viejo vector*/
-		enable();
-		viejo=getvect(0x1c) //Salvamos el viejo vector 
-		setvect(0x1c,funcionInterrupe)
 		disable();
+		viejo=getvect(0x1c); //Salvamos el viejo vector
+		setvect(0x1c,funcionInterrunpe);
+		enable();
 		/*Configuramos el timer 8253*/
 		contador=65563/(frecuencia/18.2);
-		contador_hi=frecuencia/256;
-		contador_lo=frecuencia%256;
+		contador_hi=contador&0xff00;
+		contador_hi=contador_hi/256;
+		contador_lo=frecuencia&0x00ff;
 		/*Ingresamos los valores a el contador*/
 		outportb(0x43,0x36); /*CONTROL*/
 		outportb(0x40, contador_hi);/*contador en alto*/
@@ -30,7 +43,8 @@ void configuracion(){
 }
 
 void restablecervalores(){
-		// Restablecemos el vector antiguo 
+	     printf("\nRestablecemos los valores");
+		// Restablecemos el vector antiguo
 	     disable();
 	     setvect(0x1c, viejo);
 	     // configuramos el  timer 8253
@@ -38,22 +52,42 @@ void restablecervalores(){
 	     outportb(0x40,0xFF);
 	     outportb(0x40,0XFF);
 
-	     enable();
 	}
 
-void interrupt funcionInterrupe(){
-
+void interrupt funcionInterrunpe(){
+	if (k==1)
+	{
+	    outportb(Port,0x01);
+	    //printf("Es 1- %d", inportb(Port));
+	    if(limite==17){
+		limitef++;
+		limite=0;
+	    }
+	    limite++;
+	    k=0;
+	}else{
+	    outportb(Port,0);
+	    //printf("Es 2- %d", inportb(Port));
+	    if(limite==17){
+		limitef++;
+		limite=0;
+	    }
+	    limite++;
+	    k=1;
+	}
 }
 
 int main()
-{   
-	int i=0, j=20;
-	configuracion();
-		while(i<j){
+{
+	Port=peek(0x40,0x08);
+	confi();
+	anterior=segundos+1;
+		while(limitef<20){
+		   while((inportb(Port)&(0x01))==0);
+		   while((inportb(Port)&(0x01))==1);
 
-			i++;
 		}
-
+	printf("salimos");
 	restablecervalores();
- 	return 0;
+	return 0;
 }
