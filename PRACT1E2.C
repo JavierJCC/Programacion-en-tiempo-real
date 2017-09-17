9be7fb985062e88422fb56b48282094ce9f9a8dd
JavierJCC <javisever2@gmail.com>
2017-09-17 11:32:11 -0500

    Ejercicio 4 de la practica numero 1 terminado

--- PRACT1E2.C
+++ PRACT1E2.C
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <dos.h>
+#include <graphics.h>
 
 /*Variables globales*/
-unsigned int i=1;
+unsigned int i=0;
 unsigned int segundos=0;
 unsigned int limite;
 unsigned int limitef=0;
@@ -17,18 +18,25 @@ unsigned char convh;
 unsigned char convl;
 
 /*Variables para el adq12*/
+int gdriver=DETECT;
+int gnode;
+char *numeros[40]={"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29"};
+char *temperatura[30]={"10","20","30","40","50","60","70","80","90","100","110","120"};
 unsigned int OUTBR, DIRBAS, STINR, CTREG, ADHIG, ADLOW;
 unsigned char Aalto;
 unsigned char Bbajo;
 unsigned char Cvalor;
 float VD, VA;
+float temp;
 unsigned char Cvalor2;
+float auxad=0;
 
 /*Prototipos de la funciones*/
 void confi(void);
 void restablecervalores(void);
 void interrupt far (*viejo)();
 void interrupt funcionInterrunpe();
+void coordenadasy(void);
 
 /*Configuramos el vector y realizamos el divisor de frecuencia*/
 void confi(){
@@ -54,7 +62,7 @@ void confi(){
 }
 
 void restablecervalores(){
-	     printf("\nRestablecemos los valores");
+	     //printf("\nRestablecemos los valores");
 		// Restablecemos el vector antiguo
 	     disable();
 	     setvect(0x1c, viejo);
@@ -66,6 +74,11 @@ void restablecervalores(){
 
 void interrupt funcionInterrunpe(){
 	    limite=1;
+}
+void coordenadasy(){
+ int x;
+       for(x=1;x<13;x++)
+	outtextxy(0,x*20,temperatura[x-1]);
 }
 
 int main (){
@@ -81,25 +94,39 @@ int main (){
 	/*Inicializamos todo para la convercion*/
 	   outportb(CTREG,0x00); //Selecciona el canal de 0-5v
 	   Cvalor=inportb(ADLOW); //Inicia la conversion
-		while(limitef<20){
-		if(limite==1){
-		    limite=0;
-		    limiteaux++;
-		    Aalto=inportb(ADHIG);
-		    Bbajo=inportb(ADLOW);
-		    VD=Aalto*256+Bbajo;
-		    VA=(float)VD*5/4096;
-		    printf("Se ha realizado la interrupcion %d y el voltaje es de %f\n",limiteaux,VA);
-		    if(limiteaux==val){
-		    printf("Se han realizado %d interrupciones en un segundo %d \n", limiteaux, i);
-		    limitef++;
-		    i++;
-		    limiteaux=0;
-		    }
-
+	   initgraph(&gdriver,&gnode,"C:\\BORLANDC\\BGI");
+	   coordenadasy();
+		while(limitef<21){
+			if(limite==1){ //Entra a la bandera de la interrupcion
+			    limite=0;
+			    limiteaux++;
+			    Aalto=inportb(ADHIG);
+				Bbajo=inportb(ADLOW);
+				VD=Aalto*256+Bbajo;
+				VA=(float)VD*5/4096;
+				temp=VA*24;
+					if(temp<65){ 
+					outportb(OUTBR,0x08);
+					outportb(OUTBR,0x01);
+					}
+					if(temp>=85){  //Posibles datos no se vean en la grafica ya que estan dentro de la interrupcion
+					  outportb(OUTBR,0x01);
+					  outportb(OUTBR,0x00);
+					}else if(temp>=65 && temp<85){
+					  outportb(OUTBR,0x09);
+					  outportb(OUTBR,0x08);
+					}
+						    if(limiteaux==val){		//Al termino de un segundo grafica
+							    line(i*30,auxad,(1+i)*30,temp*2);
+							    outtextxy(i*30,0,numeros[i]);
+							    auxad=temp*2;
+							    limitef++;
+							    i++;
+							    limiteaux=0;
+						    }
+			}
 		}
-		}
-	printf("salimos");
+	//printf("salimos");
 	restablecervalores();
 	}else{
 	   printf("La frecuencia no es la adecuada");
